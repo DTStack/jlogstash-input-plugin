0787ee9323d694c99b9884cff92057f15aad6e6d
sishu@dtstack.com <sishu@dtstack.com>
2016-12-13 17:32:52 +0800

    jdbc

---
+++ jdbc/src/main/java/com/dtstack/logstash/inputs/Jdbc.java
@@ -0,0 +1,34 @@
+package com.dtstack.logstash.inputs;
+
+import java.util.Map;
+
+import com.dtstack.logstash.inputs.BaseInput;
+
+@SuppressWarnings("serial")
+public class Jdbc extends BaseInput{
+
+	@SuppressWarnings("rawtypes")
+	public Jdbc(Map config) {
+		super(config);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void release() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
