afc5a5344747002643576a710d498d17487f3b0c
245730400@qq.com <245730400@qq.com>
2017-06-15 12:13:36 +0800

    新建redis项目

---
+++ redis/src/main/java/com/dtstack/jlogstash/input/Redis.java
@@ -0,0 +1,31 @@
+package com.dtstack.jlogstash.input;
+
+import com.dtstack.jlogstash.inputs.BaseInput;
+
+import java.util.Map;
+
+/**
+ * Date: 2017年6月15日
+ * Company: www.dtstack.com
+ * @author jiangbo
+ */
+
+@SuppressWarnings("serial")
+public class Redis extends BaseInput {
+
+    public Redis(Map config){
+        super(config);
+    }
+
+    public void prepare() {
+
+    }
+
+    public void emit() {
+
+    }
+
+    public void release() {
+
+    }
+}
